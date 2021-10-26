174422c08b9401ce0315effb69d9338afc14408e
dewarian <nathanbommezijn@gmail.com>
2021-10-26 15:52:04 +0200

    :fire: cleansing of comment hell

--- index.js
+++ index.js
@@ -1,72 +1,65 @@
-// /* 
-// TO DO
-// [] Lowercase all textual answers
-// [] replace undefined with 'nvt'
-// [] format dates to american date
-// [] Fix the code to be up to code standards (never change initial datasets)
+/* 
+  TO DO
+  [] Lowercase all textual answers
+  [] replace undefined with 'nvt'
+  [] format dates to american date
+  [] Fix the code to be up to code standards (never change initial datasets)
 
-// [] Create website to show progress
-// formulate an answer e.g. person has COLOR eyes and wants to become a FUTUREJOB
-// COLOR eyes likes to eat / drink Dairy in the DIRECTION WIND.
+/*
+  [] Create website to show progress
+  formulate an answer e.g. person has COLOR eyes and wants to become a FUTUREJOB
+  COLOR eyes likes to eat / drink Dairy in the DIRECTION WIND.
+*/
+// Initializing hoisted variables
+const DATASET = require('./tech-track-dataset.json')
+const PROCESS = require('./process.json')
 
-// */
-// // Initializing hoisted variables
-// const DATASET = require('./tech-track-dataset.json')
-// const PROCESS = require('./process.json')
 
 
+// Count amount entries
+function countAllEntries() {
+  return console.log(`Er zijn ${DATASET.length} entries`)
+}
 
-// // Count amount entries
-// function countAllEntries() {
-//   return console.log(`Er zijn ${DATASET.length} entries`)
+// Read whole object
+function readOneFullObject(data) {
+  console.log(data[0])
+  return data[0]
+}
+
+// function printAll(data) {
+//   return Object.entries(data).forEach(([key, value]) => console.log(``))
 // }
 
-// // Read whole object
-// function readOneFullObject(data) {
-//   console.log(data[0])
-//   return data[0]
-// }
+// Get specific object's question
+function getSpecificDataValue(key, value) {
+  console.log(DATASET[key][value])
+  return DATASET[key][value]
+}
 
-// // function printAll(data) {
-// //   return Object.entries(data).forEach(([key, value]) => console.log(``))
-// // }
+/* Get all entries with specific question
+Currently console logs instead of returning it
+*/
+const getAllValues = (question) => {
+  let permutableData = Object.assign({}, DATASET);
+  // console.log(permutableData)
+  return Object.entries(permutableData).forEach(([key, value]) => console.log(`${key}, ${toLowerCase(value[question])}`))
+}
 
-// // Get specific object's question
-// function getSpecificDataValue(key, value) {
-//   console.log(DATASET[key][value])
-//   return DATASET[key][value]
-// }
+function test(question) {
+  let permutableData = Object.assign({}, DATASET);
+  // console.log(Object.values(permutableData))
+  DATASET.map(
+    // For each element in the JSON FILE DO X
+    x => {console.log(`${toLowerCase(Object.values(x))}`)}
+  )
+}
 
-// /* Get all entries with specific question
-// Currently console logs instead of returning it
-// */
-// const getAllValues = (question) => {
-//   let permutableData = Object.assign({}, DATASET);
-//   // console.log(permutableData)
-//   return Object.entries(permutableData).forEach(([key, value]) => console.log(`${key}, ${toLowerCase(value[question])}`))
-// }
+// Transform data to lowercase
+const toLowerCase = data => {
+  return data.toLowerCase()
+}
 
-// function test(question) {
-//   let permutableData = Object.assign({}, DATASET);
-//   // console.log(Object.values(permutableData))
-//   DATASET.map(
-//     // For each element in the JSON FILE DO X
-//     x => {console.log(`${toLowerCase(Object.values(x))}`)}
-//   )
-// }
-
-// // Transform data to lowercase
-// const toLowerCase = data => {
-//   return data.toLowerCase()
-// }
-
-// // getAllValues('Kies zelf of je deze vraag beantwoord.')
-// // readOneFullObject(DATASET)
-// test()
-
-import fetch from 'node-fetch';
-
-const response = await fetch('../tech-track-dataset.json');
-const data = await response.json();
-
-console.log(data);
+// getAllValues('Kies zelf of je deze vraag beantwoord.')
+// readOneFullObject(DATASET)
+test()
