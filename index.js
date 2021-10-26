0c4ef27ff6152bf5b7625eadfda1b17a2ab8d15c
dewarian <nathanbommezijn@gmail.com>
2021-10-26 15:48:56 +0200

    :poop: Attempt for fetch
    Didn't work out because of absolute / relative URLS. Can be fixed by using hosted files

--- index.js
+++ index.js
@@ -1,48 +1,72 @@
-/* 
-TO DO
-[] Lowercase all textual answers
-[] replace undefined with 'nvt'
-[] format dates to american date
-[] Fix the code to be up to code standards (never change initial datasets)
+// /* 
+// TO DO
+// [] Lowercase all textual answers
+// [] replace undefined with 'nvt'
+// [] format dates to american date
+// [] Fix the code to be up to code standards (never change initial datasets)
 
-[] Create website to show progress
-formulate an answer e.g. person has COLOR eyes and wants to become a FUTUREJOB
-COLOR eyes likes to eat / drink Dairy in the DIRECTION WIND.
+// [] Create website to show progress
+// formulate an answer e.g. person has COLOR eyes and wants to become a FUTUREJOB
+// COLOR eyes likes to eat / drink Dairy in the DIRECTION WIND.
 
-*/
-// Initializing hoisted variables
-const DATASET = require('./tech-track-dataset.json')
-const PROCESS = require('./process.json')
+// */
+// // Initializing hoisted variables
+// const DATASET = require('./tech-track-dataset.json')
+// const PROCESS = require('./process.json')
 
-// Count amount entries
-function countAllEntries() {
-  return console.log(`Er zijn ${DATASET.length} entries`)
-}
 
-// Read whole object
-function readOneFullObject(data) {
-  console.log(data[0])
-  return data[0]
-}
 
-// Get specific object's question
-function getSpecificDataValue(key, value) {
-  console.log(DATASET[key][value])
-  return DATASET[key][value]
-}
+// // Count amount entries
+// function countAllEntries() {
+//   return console.log(`Er zijn ${DATASET.length} entries`)
+// }
 
-/* Get all entries with specific question
-Currently console logs instead of returning it
-*/
-const getAllValues = (question) => {
-  let permutableData = Object.assign({}, DATASET);
-  // console.log(permutableData)
-  return Object.entries(permutableData).forEach(([key, value]) => console.log(`${key}, ${toLowerCase(value[question])}`))
-}
+// // Read whole object
+// function readOneFullObject(data) {
+//   console.log(data[0])
+//   return data[0]
+// }
 
-// Transform data to lowercase
-const toLowerCase = data => {
-  return data.toLowerCase()
-}
+// // function printAll(data) {
+// //   return Object.entries(data).forEach(([key, value]) => console.log(``))
+// // }
 
-getAllValues('Kies zelf of je deze vraag beantwoord.')
+// // Get specific object's question
+// function getSpecificDataValue(key, value) {
+//   console.log(DATASET[key][value])
+//   return DATASET[key][value]
+// }
+
+// /* Get all entries with specific question
+// Currently console logs instead of returning it
+// */
+// const getAllValues = (question) => {
+//   let permutableData = Object.assign({}, DATASET);
+//   // console.log(permutableData)
+//   return Object.entries(permutableData).forEach(([key, value]) => console.log(`${key}, ${toLowerCase(value[question])}`))
+// }
+
+// function test(question) {
+//   let permutableData = Object.assign({}, DATASET);
+//   // console.log(Object.values(permutableData))
+//   DATASET.map(
+//     // For each element in the JSON FILE DO X
+//     x => {console.log(`${toLowerCase(Object.values(x))}`)}
+//   )
+// }
+
+// // Transform data to lowercase
+// const toLowerCase = data => {
+//   return data.toLowerCase()
+// }
+
+// // getAllValues('Kies zelf of je deze vraag beantwoord.')
+// // readOneFullObject(DATASET)
+// test()
+
+import fetch from 'node-fetch';
+
+const response = await fetch('../tech-track-dataset.json');
+const data = await response.json();
+
+console.log(data);
