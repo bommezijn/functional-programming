ddd7e498d30433984fdeb8b0ffabbcfbdea2a5c1
dewarian <nathanbommezijn@gmail.com>
2021-10-27 15:50:57 +0200

    :recycle: refactor functions and comments

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 /* 
+  formulate an answer e.g. person has COLOR eyes and wants to become a FUTUREJOB
+  COLOR eyes likes to eat / drink Dairy in the DIRECTION WIND.
+
   TO DO
   [] Lowercase all textual answers
   [] replace undefined with 'nvt'
   [] format dates to american date
   [] Fix the code to be up to code standards (never change initial datasets)
-
-/*
-  [] Create website to show progress
-  formulate an answer e.g. person has COLOR eyes and wants to become a FUTUREJOB
-  COLOR eyes likes to eat / drink Dairy in the DIRECTION WIND.
 */
+
 // Initializing hoisted variables
 const DATASET = require('./tech-track-dataset.json')
-const PROCESS = require('./process.json')
+const SAMPLE = require('./sample.json')
 
-// Count amount entries
-function countAmountEntries() {
-  return console.log(`Er zijn ${DATASET.length} entries`)
+/**
+ * @title Count the amount of entries
+ * @param {JSON} dataset JSON format file
+ * @returns {string} string amount of entries
+ */
+function countAmountEntries(dataset) {
+  return console.log(`Er zijn ${dataset.length} entries`)
 }
 
-// Read whole object
+/**
+ * @title Read a single object from file
+ * @param {JSON} dataset JSON format file 
+ * @returns {Object} a single item from the file
+ */
 function readOneFullObject(data) {
   console.log(data[0])
   return data[0]
@@ -30,8 +37,13 @@ function readOneFullObject(data) {
 // }
 
 // Get specific object's question
+/**
+ * @title Get a specific form's question response back.
+ * @param {Number} key Which object you specifically want
+ * @param {String} value Question which you want the response from 
+ * @returns 
+ */
 function getSpecificDataValue(key, value) {
-  console.log(DATASET[key][value])
   return DATASET[key][value]
 }
 
@@ -48,7 +60,13 @@ const getAllValuesFromQuestion = (question) => {
 //replace empty answers
 const fillEmptyResponse = (dataset) => {
   return dataset.map(
-    test => {return Object.values(test)}
+    test => {
+      console.log(typeof(test))
+      // test.forEach(element => {
+      //   element.toLowerCase()
+      // });
+      return test
+    }
   )
 }
 
@@ -57,6 +75,7 @@ const toLowerCase = data => {
   return data.toLowerCase()
 }
 
-console.log(fillEmptyResponse(DATASET))
+console.log(fillEmptyResponse(SAMPLE))
+console.log(getSpecificDataValue(0,'Wat is je oogkleur?'))
 // getAllValues('Kies zelf of je deze vraag beantwoord.')
 // readOneFullObject(DATASET)
