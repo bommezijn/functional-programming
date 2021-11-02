14108cf7b4e7c28b7d3691892a0fdb3f20c451e9
dewarian <nathanbommezijn@gmail.com>
2021-11-02 11:16:26 +0100

    :fire: removal of comments and moving functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* 
+/*
+  DeepDL translate color names to english names and convert to hex
   formulate an answer e.g. person has COLOR eyes and wants to become a FUTUREJOB
   COLOR eyes likes to eat / drink Dairy in the DIRECTION WIND.
 */
@@ -11,18 +12,19 @@ const SAMPLE = require('./sample.json')
 Currently console logs instead of returning it
 */
 const getAllValuesFromQuestion = (question) => {
-  let permutableData = Object.assign({}, DATASET);
-  // console.log(permutableData)
+  const permutableData = Object.assign({}, DATASET)
   return Object.entries(permutableData).forEach(([key, value]) => console.log(`${key}, ${utilities.toLowerCase(value[question])}`))
 }
 
+/**
+ * @title Get a specific form's question response back.
+ * @param {Number} key Which object you specifically want
+ * @param {String} value Question which you want the response from
+ * @returns
+ */
+function getSpecificDataValue (key, value) {
+  return DATASET[key][value]
+}
 
-console.log(utilities.replaceEmptyValue(getAllValuesFromQuestion('Wat is je oogkleur?')))
+// console.log(utilities.replaceEmptyValue(getAllValuesFromQuestion('Wat is je oogkleur?')))
 
-// console.log(fillEmptyResponse(SAMPLE))
-// console.log(getSpecificDataValue(0,'Wat is je oogkleur?'))
-// getAllValues('Kies zelf of je deze vraag beantwoord.')
-// readOneFullObject(SAMPLE)
-// console.log(getAllValuesFromQuestion('Wat is je favoriete soort huisdier?'))
-// console.log(getAllValuesFromQuestion('Kies zelf of je deze vraag beantwoord.'))
-// DeepDL translate color names to english names and convert to hex
