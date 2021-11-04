d1ec73567484fc4e0ce4192e0a60585edfec5441
dewarian <nathanbommezijn@gmail.com>
2021-11-04 11:15:29 +0100

    :fire: remove old getData function comment

--- modules/util.js
+++ modules/util.js
@@ -9,12 +9,6 @@ const fetch = require('cross-fetch')
  * @param {String} string URL to the API
  * @returns {Object} Data object in JSON format
  */
-// const getData = async (URL) => {
-//   return await fetch(URL)
-//     .then(response => response.json())
-//     .catch(error => console.error(error))
-// }
-
 const getData = async (URL) => {
   try {
     let response = await fetch(URL)
